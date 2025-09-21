790483f2e164a34c24fdcab819fbabbe4b0d1abc
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-09-21 10:32:56 +0530

    fix: improve TypeScript error comment and fix string concatenation in NavigationBar

--- components/navigation-bar/NavigationBar.tsx
+++ components/navigation-bar/NavigationBar.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-//@ts-expect-error
+// @ts-expect-error: importing SVG as a module, TypeScript needs declaration file
 import HonoSvg from "@/assets/svgs/hono.svg";
 
 const NavigationBar = (props: { className?: string }) => {
   return (
-    <div className={"container mx-auto" + props.className ? props.className : ""}>
+    <div className={"container mx-auto" + (props.className ? props.className : "")}>
       <div className="flex flex-row justify-between items-center">
         <div className="flex flex-row gap-2 items-center">
-          <Image src={HonoSvg} alt="Hono's Logo" className="w-6 h-6" />
+          <Image src={HonoSvg} alt="Hono&apos;s Logo" className="w-6 h-6" />
           <span className="text-2xl font-bold">Hono</span>
         </div>
         <div>
